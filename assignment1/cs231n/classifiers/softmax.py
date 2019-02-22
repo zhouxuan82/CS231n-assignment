0fed6ebce0db2b9f44b057a4eba6fb57dbc2acd0
zhou_shengping <zhou_shengping@newcontrailer.com>
2019-02-22 18:08:04 +0800

    softmax done

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,33 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  num_class = W.shape[1]
+  num_train = X.shape[0]
+
+  for i in range(num_train):
+
+    scores = np.dot(X[i], W)
+    scores -= np.max(scores)
+
+    current_class_score = scores[y[i]]
+
+    # loss += - np.log(np.exp(current_class_score) / np.sum(np.exp(scores)))
+    loss += - current_class_score + np.log(np.sum(np.exp(scores)))
+
+    for j in range(num_class):
+
+      dW[:, j] += np.dot(np.exp(scores[j]), X[i].T) / np.sum(np.exp(scores))
+
+      if j == y[i]:
+        dW[:, y[i]] -= X[i].T
+
+  loss /= num_train
+  loss += reg * np.sum(W * W)
+
+  dW /= num_train
+  dW += 2 * reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +79,27 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  # loss
+  num_train = X.shape[0]
+  scores = X.dot(W)   # 200 x 3073 * 3073 x 10 = 200 x 10
+  # scores -= np.transpose([np.max(scores, axis=1)])
+  scores -= np.max(scores, axis=1)[:, np.newaxis]
+
+  scores_label = scores[[range(num_train)], y] # 1 x 200
+  loss_array = - scores_label + np.log(np.sum(np.exp(scores), axis=1)) # 200 x 10
+
+  loss = np.sum(loss_array) / num_train + reg * np.sum(W * W)
+
+  # dW
+  scores_probs = np.exp(scores) / np.sum(np.exp(scores), axis=1)[:, np.newaxis]
+  scores_probs[range(num_train), y] -= 1
+  dW = np.dot(X.T, scores_probs)
+
+  dW /= num_train
+  dW += 2 * reg * W
+
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
